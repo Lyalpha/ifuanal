164fac13803133f3e6a42b86f835df3b2a8c22ca
jdl <joedlyman@gmail.com>
2016-09-14 11:44:14 +0100

    Fix docstring formatting

--- ifuanal.py
+++ ifuanal.py
@@ -2,28 +2,8 @@
 IFUANAL
 
 For the analysis of IFU data cubes.
-
-
-Copyright (c) 2016 Joe Lyman
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the “Software”), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
 """
+
 from __future__ import print_function
 
 __version__ = "0.3.0"
@@ -476,8 +456,8 @@ class IFUCube(object):
         Apply the HII explorer [SFS]_ binning algorithm to the datacube.
 
         This method will bin spaxels by attempting to determine distinct
-        emission line regions. An emission line map (usually H:math:`\\alpha`)
-        is created by :func:`~ifuanal.get_line_map`` through subtraction of a
+        emission line regions. An emission line map (usually H\ :math:`\\alpha`)
+        is created by :func:`~ifuanal.get_line_map` through subtraction of a
         continuum from a narrow band filter centred on the emission line. Peaks
         above ``min_peak_flux`` in the emission line map are seeds for bins
         which are expanded so long as neighbouring spaxels are above
